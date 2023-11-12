9fb3a4a3ff19057237bdc81ae6f6d1b3f5f0fc24
elul <scorpiondota22@gmail.com>
2023-11-12 23:30:17 +0300

    added lazyloading to the routes created a loading fallback component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import Home from "./Pages/Home";
 import Nav from "./components/Nav";
-import DesktopFireFox from "./Pages/DesktopFireFox";
 import { Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loading from "./components/Loading";
+const DesktopFireFox = lazy(() => import("./Pages/DesktopFireFox"));
 const App = () => {
     return (
-        <div>
+        <div className='relative'>
             <Nav />
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/DesktopFireFox' element={<DesktopFireFox />} />
+                <Route
+                    path='/DesktopFireFox'
+                    element={
+                        <Suspense fallback={<Loading />}>
+                            <DesktopFireFox />
+                        </Suspense>
+                    }
+                />
             </Routes>
         </div>
     );
